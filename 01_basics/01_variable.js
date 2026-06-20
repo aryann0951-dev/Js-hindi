4333e4c6f628c085bc9b65f77f753031bdb04f50
Aryan <aryann0951@gmail.com>
2026-06-20 14:29:39 +0000

    session_2

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,28 @@
+const  accountId = 144553 
+let accountEmail = "aryan@gmail.com"
+var accountPassword = "122345"
+accountCity = "Haryana"
+
+//accountId = 2
+console.log(accountId);
+
+accountEmail = "aryanquantish@gmail.com"
+accountPassword = "212121"
+accountCity = "gurgaon"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+let accountState; 
+console.log(accountState);
+
+"use strict" // treat all js code as newer version 
+
+//alert(3+3)// we are using nodejs , not browser 
+
+console.log(3+3) // code redability should be high
+
+
+console.log("aryan")
+
+
+
